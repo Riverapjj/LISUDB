5a31eff6ae6576c0a9dee1a421319fe73b60f61b
Josué Rivera <keko.rai.jr@gmai.com>
2022-02-08 22:05:09 -0600

    Validacion de errores

--- Practica4/procesar.php
+++ Practica4/procesar.php
@@ -9,32 +9,34 @@ require 'validaciones.php';
 
         if (!isset($nombres) || estaVacio($nombres)) {
             array_push($errores, "Debes ingresar el nombre");
-            if (!esTexto($nombres)) {
-                array_push($errores, "El nombre debe contener solo letras");
-            }
+        }else if (!esTexto($nombres)) {
+            array_push($errores, "El nombre solo debe contener letras");
         }
 
         if (!isset($apellidos) || estaVacio($apellidos)) {
             array_push($errores, "Debes ingresar los apellidos");
-            if (!esTexto($nombres)) {
-                array_push($errores, "El apellido debe contener solo letras");
-            }
+        }else if (!esTexto($apellidos)) {
+            array_push($errores, "El apellido solo debe contener letras");
         }
 
-        if (!isset($telefono) || estaVacio($telefono)) {
-            array_push($errores, "Debes ingresar el telefono"); 
-            if (!esMail($correo)) {
-                array_push($errores, "Correo no válido");
-            }
+        if (!isset($correo) || estaVacio($correo)) {
+            array_push($errores, "Debes ingresar el correo"); 
+        }else if (!esMail($correo)) {
+            array_push($errores, "Correo no válido");
         }
        
         if (!isset($telefono) || estaVacio($telefono)) {
             array_push($errores, "Debes ingresar el telefono"); 
+        }else if (!esTelefono($telefono)) {
+            array_push($errores, "Teléfono no válido");
         }
 
         if (!isset($carnet) || estaVacio($carnet)) {
             array_push($errores, "Debes ingresar el carnet");
+        }else if (!esTelefono($telefono)) {
+            array_push($errores, "Carnet no válido");
         }
+
         var_dump($errores);
     }
 
--- Practica4/validaciones.php
+++ Practica4/validaciones.php
@@ -11,4 +11,12 @@
     function esMail($var) {
         return filter_var($var, FILTER_VALIDATE_EMAIL);
     }
+
+    function esCarnet($var) {
+        return preg_match('/^[A-Z]{2}[0-9]{6}$/', $var);
+    }
+
+    function esTelefono($var) {
+        return preg_match('/^[762][0-9]{3}-?[0-9]{4}$/', $var);
+    }
 ?>
